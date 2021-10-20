cb2399fa3f96794c033bb4c598fdf10123c6b78e
abderrahmanriag <r.dahman47@gmail.com>
2021-10-20 21:26:47 +0100

    all changes done!

--- said.py
+++ said.py
@@ -1,5 +1,11 @@
 from tkinter import *
+import test_abd
 root = Tk()
+
+def add():
+    x=entryNumberX.get()
+    y=entryNumberY.get()
+    entryEqual.insert(0, test_abd.add(x, y))
 entryNumberX = Entry(root, show=None, font=('Arial', 25))
 entryNumberX.place(relx=0.25, rely=0.2, relwidth=0.2, relheight=0.1)
 
@@ -10,7 +16,7 @@ entryNumberY.place(relx=0.55, rely=0.2, relwidth=0.2, relheight=0.1)
 entryEqual = Entry(root, show=None, font=('Arial', 25))
 entryEqual.place(relx=0.35, rely=0.4, relwidth=0.3, relheight=0.1)
 
-buttonAddition = Button(root, text="Addition")
+buttonAddition = Button(root, text="Addition", command=add)
 buttonAddition.place(relx=0.35, rely=0.6, relwidth=0.3, relheight=0.1)
 
 
